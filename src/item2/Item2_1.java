bb7502deeb4e1655379914e27749099a35e9993d
hun9003 <hun9003@naver.com>
2022-09-26 22:32:11 +0900

    빌더 패턴의 예시와 핵심 정리

--- src/item2/Item2_1.java
+++ src/item2/Item2_1.java
@@ -33,8 +33,104 @@ public class Item2_1 {
         Pizza(Builder<?> builder) {
             toppings = builder.toppings.clone();
         }
+    }
+    /*
+        Pizza.Builder 클래스는 재귀적 타입 한정을 이용하는 제네릭 타입이다.
+        여기에 추상 메서드인 self를 더해 하위 클래스에서는 형변환하지 않고도 메서드 연쇄를 지원할 수 있다.
+        self 타입이 없는 자바를 위한 이 우회 방법을 시뮬레이트한 셀프 타입(simulated self-type) 관용구라 한다.
+     */
 
+    /*
+        여기 Pizza의 하위 클래스가 2개 있다. 하나는 일반적인 뉴욕 피자이고,
+        다른 하나는 칼초네 (calzone) 피자다. 뉴욕 피자는 크기(size) 매개변수를 필수로 받고.
+        칼초네 피자는 소스를 안에 넣을지 선택(sauceInside)하는 매개변수를 필수로 받는다.
+     */
 
+    // 뉴욕 피자
+    public static class NyPizza extends Pizza {
+        public enum Size { SMALL, MEDIUM, LARGE }
+        private final Size size;
+
+        public static class Builder extends Pizza.Builder<Builder> {
+            private final Size size;
+
+            public Builder(Size size) {
+                this.size = Objects.requireNonNull(size);
+            }
+
+            @Override
+            public NyPizza build() {
+                return new NyPizza(this);
+            }
+
+            @Override
+            protected Builder self() {return this;}
+        }
+
+        private NyPizza(Builder builder) {
+            super(builder);
+            size = builder.size;
+        }
+    }
+
+    // 칼초네 피자
+    public static class Calzone extends Pizza {
+        private final boolean sauceInside;
+
+        public static class Builder extends Pizza.Builder<Builder> {
+            private boolean sauceInside = false; // 기본값
+
+            public Builder sauceInside() {
+                sauceInside = true;
+                return this;
+            }
+
+            @Override
+            public Calzone build() {
+                return new Calzone(this);
+            }
+
+            @Override
+            protected Builder self() {
+                return this;
+            }
+        }
+        private Calzone(Builder builder) {
+            super(builder);
+            sauceInside = builder.sauceInside;
+        }
+    }
+
+    /*
+        각 하위 클래스의 빌더가 정의한 build 메서드는 해당하는 구체 하위 클래스를 반환하도록 선언한다.
+        NyPizza.Builder 는 NyPizza를 반환하고 Calzone.Builder는 Calzone를 반환한다는 뜻이다.
+        하위 클래스의 메서드가 상위 클래스의 메서드가 정의한 반환 타입이 아닌,
+        그 하위 타입을 반환하는 기능을 공변 반환 타이핑 (covariant return typing) 이라 한다.
+        이 기능을 이용하면 클라이언트가 형변환에 신경 쓰지 않고도 빌더를 사용할 수 있다.
+     */
+
+    /*
+        이러한 '계층적 빌더' 를 사용하는 클라이언트의 코드도 앞선 영양정보 빌더를 사용하는 코드와 다르지 않다.
+     */
+    public static void main(String[] args) {
+        NyPizza pizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE).addTopping(Pizza.Topping.ONION).build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(Pizza.Topping.HAM).sauceInside().build();
+
+        /*
+            생성자로는 누릴 수 없는 사소한 이점으로, 빌더를 이용하면 가변 인수(varargs) 매개변수를 여러 개 사용할 수 있다.
+            각각을 적절한 메서드로 나워 선언하면 된다.
+            아니면 메서드를 여러 번 호출하도록 하고 각 호출 때 넘겨진 매개변수들을 하나의 필드로 모을 수도 있다.
+         */
     }
 }
+
+/*
+    핵심 정리
+    생성자나 정적 팩터리가 처리해야 할 매개변수가 많다면 빌더 패턴을 선택하는 게 더 낮다.
+    매개변수 중 다수가 필수가 아니거나 같은 타입이면 특히 더 그렇다.
+    빌더는 점층적 생성자보다 클라이언트 코드를 읽고 쓰기가 훨씬 간결하고, 자바빈즈보다 훨씬 안전하다.
+ */
 
